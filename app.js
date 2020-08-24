461770f2b1fc24fcaeb91d611916f6d37c712cd5
leogirardeau <leogir.pro@gmail.com>
2020-08-24 11:05:03 +0200

    making it responsive

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+const header = document.querySelector("header");
+const menuToggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const navs = document.querySelectorAll(".menu li a");
+
 window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+menuToggle.addEventListener("click", toggleMenu);
+
+navs.forEach((nav) => {
+  nav.addEventListener("click", toggleMenu);
+});
+
+function toggleMenu() {
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
